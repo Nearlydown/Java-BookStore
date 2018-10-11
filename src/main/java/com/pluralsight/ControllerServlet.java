40acb96b6aca8767c3571e78a9a8977ef602d9d0
Andy Lyon <andy.lyon@me.com>
2018-10-11 17:53:39 +0100

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -59,6 +59,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/insert":
 					insertBook(request, response);
           break;
+				case "/delete":
+					deleteBook(request, response);
+          break;
         default:
 				   listBooks(request, response);
            break;
@@ -67,6 +70,16 @@ public class ControllerServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		bookDAO.deleteBook(id);
+		
+		response.sendRedirect("list");
+		
 	}
 
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
