1923cbe14de16ed29bd8605200425270ca66c630
Andy Lyon <andy.lyon@me.com>
2018-10-12 11:59:51 +0100

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 					deleteBook(request, response);
           break;
+				case "/edit":
+					showEditForm(request, response);
+          break;
         default:
 				   listBooks(request, response);
            break;
@@ -80,6 +83,17 @@ public class ControllerServlet extends HttpServlet {
 		
 		response.sendRedirect("list");
 		
+	}
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		Book book = bookDAO.getBook(id);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", book);
+		requestDispatcher.forward(request, response);
 	}
 
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
